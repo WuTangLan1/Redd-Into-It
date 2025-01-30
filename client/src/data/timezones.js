8b8d1d25ff2953730b19b504ea72d6e2fb300ed1
WuTangLan1 <finnmassari16@gmail.com>
2025-01-30 15:36:52 +0200

    + debounce hook

---
+++ client/src/data/timezones.js
@@ -0,0 +1,21 @@
+// client/src/data/timezones.js
+
+export default [
+    'UTC',
+    'America/New_York',
+    'America/Chicago',
+    'America/Denver',
+    'America/Los_Angeles',
+    'Europe/London',
+    'Europe/Berlin',
+    'Asia/Tokyo',
+    'Asia/Shanghai',
+    'Australia/Sydney',
+    'America/Sao_Paulo',
+    'Asia/Kolkata',
+    'Africa/Johannesburg',
+    'Asia/Dubai',
+    'Pacific/Auckland',
+    // Add more as needed
+  ];
+  
