40af42263c6a371bcbe5e01cd51c9720a5bd56fa
WuTangLan1 <finnmassari16@gmail.com>
2025-01-30 13:48:19 +0200

    added .envs

--- server/venv/Scripts/activate.bat
+++ server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\OneDrive\Documents\GitHub\reddintoit\server\venv
+set VIRTUAL_ENV=C:\Users\User\onedrive\desktop\github\vue3_pinia_secretsanta\redd-into-it\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
