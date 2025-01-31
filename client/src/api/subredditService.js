4cde388d6f5a0c683de5d7f2284a822c82c8a44d
WuTangLan1 <finnmassari16@gmail.com>
2025-01-31 12:31:40 +0200

    more logging

--- client/src/api/subredditService.js
+++ client/src/api/subredditService.js
@@ -4,11 +4,14 @@ import axios from './axiosConfig';
 
 export const searchSubreddits = async (query) => {
   try {
-    const response = await axios.get('/subreddit/search', {
-      params: { q: query },
-    });
+    console.log("Making API call to subreddit search:", query);
+    console.log(import.meta.env.VITE_API_BASE_URL)
+
+    const response = await axios.get(`/subreddit/search`, { params: { q: query } });
+    console.log("API Response:", response.data);
     return response.data.results;
   } catch (error) {
+    console.error("Search Subreddit Error:", error.response?.data || error.message);
     throw error.response?.data?.error || 'An unexpected error occurred.';
   }
 };
